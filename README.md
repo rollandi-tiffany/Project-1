4703b5e6bcb472f329c01e78a3146fe2b9d3f041
Tiffany Torres <torresrtiffany@gmail.com>
2023-06-28 17:53:25 -0700

    completed step 2, setting a random square to green

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Project 1- HTML/JS/CSS Project-1
 ## Description
 This is a game made from HTML, JS & CSS.
 
-##Notes
+## Notes
 
 ## Making a commit (basic workflow)
 1. make changes
@@ -13,6 +13,7 @@ this "stages" all changes for commit
 3. `git commit -m "initial page load with grid and leaderboard`
 this makes the literal commit
 4. `git push origin main`
+this pushes the commit to the 
 
 
 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-    console.log("I'm ready!")
+    //select the start button
+    const $startButton = $("#start");
+    const $squares = $(".square");
+    //listen for clicks on the starte button
+    $startButton.on("click", function(){
+        randomSquare();
+    });
+
+    //when clicked, call a function that selections a random square that turns the background green
+    function randomSquare(){
+        const randomSquare = $squares[Math.floor(Math.random() * $squares.length)];
+        $(randomSquare).addClass("green-bg");
+
+        setTimeout(function(){
+            $(randomSquare).removeClass("green-bg");
+
+        }, 3000)
+
+    }
 });
